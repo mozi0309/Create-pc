339fa5d8337afd8c1ac922929e78b1161d1abb96
lisa <lisa_zhangll@163.com>
2019-07-01 18:22:23 +0800

    feat: 脚手架

---
+++ src/app/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { hot } from 'react-hot-loader/root'
+
+
+class App extends React.Component {
+  render () {
+    return (
+      <div>
+        <h1>Hello, Lisa</h1>
+      </div>
+    )
+  }
+}
+
+export default hot(App)
